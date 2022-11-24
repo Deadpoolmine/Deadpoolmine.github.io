00964beee88752c4c8926784f766ed02dd25cf2b
Pan Yanqi <2841954631@qq.com>
2022-11-24 19:55:44 +0800

    deploy zhaoo theme

---
+++ deploy.bat
@@ -0,0 +1,14 @@
+@echo off
+
+if "%~1" == "" (
+    echo Usage: deploy.bat "Description of the deployment"
+    exit /b 1
+)
+
+mkdir "%CD%\..\themes_git_tmemp"
+mv "%CD%\themes\zhaoo\.git" "%CD%\..\themes_git_tmemp"
+git "add" "."
+git "commit" "-m" "%~1"
+git "push" "origin" "main"
+mv "%CD%\..\themes_git_tmemp\.git" "./themes/zhaoo"
+DEL /S "%CD%\..\themes_git_tmemp"
